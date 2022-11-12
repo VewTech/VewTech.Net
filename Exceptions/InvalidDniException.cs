ca74ce4e4e2e01860a5f880f6ad61f14868c0d41
Josep Sendra Serra <je.sendra@vewtech.net>
2022-11-12 16:14:22 +0100

    Agregar archivos de proyecto.

---
+++ Exceptions/InvalidDniException.cs
@@ -0,0 +1,8 @@
+﻿namespace VewTech.Net.Exceptions
+{
+    public class InvalidDniException : Exception
+    {
+        public InvalidDniException() : base("Input DNI is invalid.") {}
+        public InvalidDniException(string message) : base(message) {}
+    }
+}
---
+++ Validation/DniValidator.cs
@@ -0,0 +1,34 @@
+﻿using VewTech.Net.Exceptions;
+
+namespace VewTech.Net.Validation
+{
+    public static class DniValidator
+    {
+        /// <summary>
+        /// <para>Throws an InvalidDni exception with the invalidity reason as the message if DNI is invalid.</para>
+        /// <example>
+        /// For example:
+        /// <code>
+        /// try{
+        ///     Validate()
+        /// catch (InvalidDniException e){
+        ///     Debug.Write(e.Message);
+        /// }
+        /// </code>
+        /// Will output the reason why this DNI is invalid, or give no output if it is valid.
+        /// </example>
+        /// </summary>
+        public static void Validate(string dniToValidate)
+        {
+            if (string.IsNullOrEmpty(dniToValidate))
+            {
+                throw new InvalidDniException("DNI can't be empty.");
+            }
+
+            if (dniToValidate.Length != 9)
+            {
+                throw new InvalidDniException("DNI must have 9 characters");
+            }
+        }
+    }
+}
